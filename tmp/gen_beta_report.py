4da4a38c2d9b9ed4b5c2421434146d0cc9663109
Pebbl3s <james.pedley18@imperial.ac.uk>
2020-12-31 16:18:49 +0000

    Corrected python imports

--- tmp/gen_beta_report.py
+++ tmp/gen_beta_report.py
@@ -1,18 +1,20 @@
-import Mdutils
+from mdutils import MdUtils
 import sys
 
 def gen_content():
     print("Generating content")
-    mdFile = Mdutils(file_name="report", title="Beta_Report")
+    mdFile = MdUtils(file_name="report", title="Beta_Report")
     mdFile.new_paragraph("Why do phd students get holidays and we don't? :) ")
-    mdFile.create_md_file()
+    return mdFile
 
-def save_content(location):
+def save_content(file):
     print("Saving MD file")
+    file.create_md_file()
 
 def main(argvs):
-    content = gen_content()
-    #save_content(argvs[0])
+    print("path is", argvs)
+    file = gen_content()
+    save_content(file)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
