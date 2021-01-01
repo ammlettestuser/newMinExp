c4566e15c372631029289f25d5818667abc6093a
Pebbl3s <james.pedley18@imperial.ac.uk>
2021-01-01 18:27:02 +0000

    New json parsing approach

--- experiment.py
+++ experiment.py
@@ -2,7 +2,7 @@ import json
 
 import numpy as np
 import argparse
-
+import sys
 
 def main(path, N):
     a = np.random.rand(N,2)
@@ -10,20 +10,20 @@ def main(path, N):
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description='Save list of random 2D vectors in specified location ')
+    #parser = argparse.ArgumentParser(description='Save list of random 2D vectors in specified location ')
     #parser.add_argument('N', metavar='N', type=int,
     #                                        help='Number of numbers')
     
-    parser.add_argument("hparameters", type=str, help="Experiment hyper-parameters")
-    parser.add_argument('--dest', dest='path',
-                        default="./",
-                        help='Destination of the result file')
+    #parser.add_argument("hparameters", type=str, help="Experiment hyper-parameters")
+    #parser.add_argument('--dest', dest='path',
+    #                    default="./",
+    #                    help='Destination of the result file')
 
-    args = parser.parse_args()
+    #args = parser.parse_args()
     #print("The value of N is:", args.N)
-    print(args.path)
+    #print(args.path)
     
-    obj = json.loads(args.hparameters)
+    obj = json.loads(" ".join(sys.argv[1:]))
     print("Value of N is", obj["N"])
 
     main(args.path, obj["N"])
